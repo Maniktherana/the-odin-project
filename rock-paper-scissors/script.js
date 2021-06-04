59885a0193241f9b5abe81c32e9671d413dd7aca
Manik <manikrana54@gmail.com>
2021-06-04 18:30:31 +0530

    add game logic (finally)

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,8 @@
 const choices = document.querySelectorAll('.selection')
+const restart = document.getElementById('restart')
+const result = document.getElementById('result')
+const modal = document.querySelector('modal')
+
 const SELECTIONS = [
     {
       name: 'rock',
@@ -18,7 +22,28 @@ const SELECTIONS = [
 ]
 
 function play(e) {
-      console.log(e.target.id)
+    const playerSelection = SELECTIONS.find(selection => selection.name === e.target.id)
+    const compterSelection = getComputerChoice()
+    const playerChoice = playerSelection.name
+    const computerChoice = compterSelection.name
+    const win = playRound(playerSelection, compterSelection)
+    console.log(playerChoice, computerChoice, win)
+}
+
+//get computer choice
+function getComputerChoice() {
+    return SELECTIONS[Math.floor(Math.random() * 3)]
+}
+
+//decide who wins
+function playRound(choice, opponentChoice) {
+    if (choice === opponentChoice) {
+        return 'Draw'
+    } else if (choice.beats === opponentChoice.name) {
+        return 'Player Wins'
+    } else if (choice.name === opponentChoice.beats) {
+        return 'Computer Wins'
+    }
 }
 
 //Event Listener
