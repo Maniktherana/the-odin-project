979e8914b5463f06ece0e362a7245463ad290a83
Manik <manikrana54@gmail.com>
2021-06-04 19:41:19 +0530

    Add scoring logic

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,7 +2,13 @@ const choices = document.querySelectorAll('.selection')
 const restart = document.getElementById('restart')
 const result = document.getElementById('result')
 const modal = document.querySelector('modal')
-
+const playerScore = document.getElementById('playerScore')
+const compuerScore = document.getElementById('computerScore')
+const scoreBoard = {
+    player: 0,
+    computer: 0,
+    roundCount: 0
+}
 const SELECTIONS = [
     {
       name: 'rock',
@@ -21,29 +27,51 @@ const SELECTIONS = [
     }
 ]
 
+/**
+ * Carry out game logic
+ * @param {*} e 
+ */
 function play(e) {
+    scoreBoard.roundCount++
     const playerSelection = SELECTIONS.find(selection => selection.name === e.target.id)
     const compterSelection = getComputerChoice()
     const playerChoice = playerSelection.name
     const computerChoice = compterSelection.name
     const win = playRound(playerSelection, compterSelection)
-    console.log(playerChoice, computerChoice, win)
+    console.log(playerChoice, computerChoice, win, scoreBoard)
 }
 
-//get computer choice
+/**
+ * Randomly assign rock paper or scissors object from SELECTIONS to the computer
+ * @returns one of the objects from SELECTIONS
+ */
 function getComputerChoice() {
     return SELECTIONS[Math.floor(Math.random() * 3)]
 }
 
-//decide who wins
+/**
+ * Decide who wins the round and update index.html
+ * @param {playerSelection} choice 
+ * @param {computerSelection} opponentChoice 
+ * @returns Whoever wins the round
+ */
 function playRound(choice, opponentChoice) {
+    
     if (choice === opponentChoice) {
         return 'Draw'
     } else if (choice.beats === opponentChoice.name) {
+        scoreBoard.player++
+        playerScore.textContent = scoreBoard.player
         return 'Player Wins'
     } else if (choice.name === opponentChoice.beats) {
+        scoreBoard.computer++
+        computerScore.textContent = scoreBoard.computer
         return 'Computer Wins'
     }
+}
+
+function showModal(win) {
+    
 }
 
 //Event Listener
