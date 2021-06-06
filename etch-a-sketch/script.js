8dc9165d7d7ca4a7259189a03c6cf72be2df2642
Manik <manikrana54@gmail.com>
2021-06-06 16:51:39 +0530

    add grid sizing mechanics

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,86 @@
-const etch = document.querySelectorAll('.grid-item')
 
+const sizes = document.querySelectorAll('.size')
+const modes = document.querySelectorAll('.mode')
+const gridContainer = document.querySelector('.grid-container')
 
-//Event Listener
-for (let i = 0; i < etch.length; i++) {
-    etch[i].onmouseover = function(e) {
-      e.target.style.backgroundColor = 'black';
+function generateGrid(e) {
+    console.log(e.target.id)
+    if (e.target.id === 'small') {
+        smallGrid()
+    } else if (e.target.id === 'medium') {
+        mediumGrid()
+    } else if (e.target.id === 'large') {
+        largeGrid()
     }
-  }
+}
+
+function clearGrid() {
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+}
+
+function action(e) {
+    console.log(e.target.id)
+
+}
+
+function smallGrid() {  
+    clearGrid()
+    gridContainer.setAttribute('style', 'grid-template-columns: repeat(6, 1fr); grid-template-rows: repeat(6, 1fr)');    
+    for (let i = 0; i < 36; i++) {
+        let div = document.createElement('div')
+        div.classList.toggle('grid-item')
+        gridContainer.appendChild(div);
+    }
+    
+}
+
+function mediumGrid() {
+    clearGrid()
+
+    gridContainer.setAttribute('style', 'grid-template-columns: repeat(8, 1fr); grid-template-rows: repeat(8, 1fr)');    
+    for (let i = 0; i < 64; i++) {
+        let div = document.createElement('div')
+        div.classList.toggle('grid-item')
+        gridContainer.appendChild(div);
+    }
+}
+
+function largeGrid() {
+    clearGrid()
+
+    gridContainer.setAttribute('style', 'grid-template-columns: repeat(64, 1fr); grid-template-rows: repeat(64, 1fr)');    
+    for (let i = 0; i < 4096    ; i++) {
+        let div = document.createElement('div')
+        div.classList.toggle('grid-item')
+        gridContainer.appendChild(div);
+        
+    }
+}
+
+//Event listener new
+sizes.forEach(size => size.addEventListener('click', generateGrid))
+modes.forEach(mode => mode.addEventListener('click', action))
+
+const etch = document.querySelectorAll('.grid-container > div')
+etch.forEach((item) => {
+    item.count = 0
+    item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = '#707070';
+        console.log('ok')
+    })
+})
+
+//Event Listener old
+// for (let i = 0; i < etch.length; i++) {
+//     etch[i].onclick = function(e) {
+//         console.log('ok')
+//     }
+//   }
+
+function pageLoad() {
+    mediumGrid()
+}
+
+pageLoad()
