16574a0d9f7cdf662007dd1841964d18a3a3b8db
Manik <manikrana54@gmail.com>
2021-06-09 14:05:27 +0530

    Add documentation

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -13,19 +13,26 @@ const RANDOM_COLORS = ['#fc0303', '#fc5203', '#fc7f03', '#fca503', '#fcc203',
 let currentMode = GAME_MODES[0]
 let currentSize = GRID_SIZE[1]
 
+
   /**
-   * 
+   * Generate a grid when button of class size is selected and log the size to console
    * @param {*} e 
    */
 function generateGrid(e) {
+
+    // If Small is selected, make a new small sized grid
     if (e.target.id === 'small') {
         currentSize = GRID_SIZE[0]
         console.log(currentSize)
         newGrid(currentSize)
+
+    // If Medium is selected, make a new medium sized grid
     } else if (e.target.id === 'medium') {
         currentSize = GRID_SIZE[1]
         console.log(currentSize)
         newGrid(currentSize)
+
+    // If Large is selected, make a new large sized grid
     } else if (e.target.id === 'large') {
         currentSize = GRID_SIZE[2]
         console.log(currentSize)
@@ -34,8 +41,8 @@ function generateGrid(e) {
 }
 
 /**
- * 
- * @param {*} size 
+ * Create a new grid with selected size
+ * @param {string} size The current size
  */
 function newGrid(size) {
     if (size === 'small') {
@@ -48,17 +55,25 @@ function newGrid(size) {
 }
 
 /**
- * 
+ * Select a mode when button of class .mode is selected and create a new grid to draw on
  * @param {*} e 
  */
  function selectMode(e) {
+
+    // Show selected game mode
     console.log(e.target.id)
+
+    // Make a new grid of current size with game mode of Classic
     if (e.target.id === 'classic') {
         currentMode = GAME_MODES[0]
         newGrid(currentSize)
+
+    // Make a new grid of current size with game mode of Dark
     } else if (e.target.id === 'dark') {
         currentMode = GAME_MODES[1]
         newGrid(currentSize)
+
+    // Make a new grid of current size with game mode of Sprinkles
     } else if (e.target.id === 'sprinkles') {
         currentMode = GAME_MODES[2]
         newGrid(currentSize)
@@ -67,71 +82,96 @@ function newGrid(size) {
 }
 
 /**
- * 
+ * Fill grid-container with an 8x8 grid of square divs with class .grid-item.
+ * @param {string} gameMode The current game mode
  */
  function smallGrid(gameMode) {  
+
+    // Remove current grid
     eraseGrid()
+
+    // Set CSS for gridContainer
     gridContainer.setAttribute('style', 'grid-template-columns: repeat(8, 1fr); grid-template-rows: repeat(8, 1fr)');    
+    
+    // Make an 8x8 grid of square divs
     for (let i = 0; i < 64; i++) {
         let div = document.createElement('div')
+        div.setAttribute('id', i); 
         div.classList.toggle('grid-item')
         gridContainer.appendChild(div)
     }
+
+    // Color grid with current game mode
     hoverColor(gameMode)
 
 }
 
 /**
- * 
+ * Fill grid-container with an 16x16 grid of square divs with class .grid-item.
+ * @param {string} gameMode The current game mode
  */
 function mediumGrid(gameMode) {
+    
+    // Remove current grid
     eraseGrid()
 
+    // Set CSS for gridContainer
     gridContainer.setAttribute('style', 'grid-template-columns: repeat(32, 1fr); grid-template-rows: repeat(32, 1fr)');    
+    
+    // Make a 16x16 grid of square divs
     for (let i = 0; i < 1024; i++) {
         let div = document.createElement('div')
+        div.setAttribute('id', i); 
         div.classList.toggle('grid-item')
         gridContainer.appendChild(div)
     }
+    
+    // Color grid with current game mode
     hoverColor(gameMode)
 
 }
 
 /**
- * 
+ * Fill grid-container with an 64x64 grid of square divs with class .grid-item.
+ * @param {string} gameMode The current game mode
  */
 function largeGrid(gameMode) {
+    
+    // Remove current grid
     eraseGrid()
 
+    // Set CSS for gridContainer
     gridContainer.setAttribute('style', 'grid-template-columns: repeat(64, 1fr); grid-template-rows: repeat(64, 1fr)');    
+    
+    // Make a 64x64 grid of square divs
     for (let i = 0; i < 4096    ; i++) {
         let div = document.createElement('div')
+        div.setAttribute('id', i); 
         div.classList.toggle('grid-item')
         gridContainer.appendChild(div)
     }
+    
+    // Color grid with current game mode
     hoverColor(gameMode)
 
 }
 
 /**
- * 
+ * Remove any contents of .gridContainer.
  */
 function eraseGrid() {
+
+    // While gridContainer contains a .grid-item, remove that item
     while(gridContainer.firstChild){
         gridContainer.removeChild(gridContainer.firstChild)
     }
 }
 
-
-
 /**
- * 
+ * Clear the grid of any sketches when the Clear button is clicked.
  */
 function clearGrid() {
-    let items = document.querySelectorAll('.grid-item')
-    items.forEach(item => {
-        item.style.backgroundColor = 'rgb(212, 212, 212)'
-    })
+    newGrid(currentSize)
 }
 
 //Event listeners
@@ -140,22 +180,31 @@ modes.forEach(mode => mode.addEventListener('click', selectMode))
 clear.addEventListener('click', clearGrid)
 
 /**
- * 
+ * Color the square div when on mouse hover according to the selected mode.
+ * @param {string} mode The current game mode
  */
 function hoverColor(mode) {
+
+    // Select all items in gridContainer
     let items = document.querySelectorAll('.grid-item')
-    let x = 0
-    items.forEach((item, index) => {
-        item.addEventListener('mouseover', () => {
-            x++
+
+    // For each .grid-item, color on mouseover
+    items.forEach((item) => {        
+        item.addEventListener('mouseover', () => {    
+            
+            // If game mode is classic, make the div grey
             if (mode === 'classic') {
                 item.style.backgroundColor = '#414141'
-            } else if (mode === "dark") {
-                item.style.backgroundColor = 'black'
 
+            // If game mode is dark, make the div black
+            } else if (mode === "dark") {
+                item.style.backgroundColor = `rgba(0 , 0, 0, .999)`
+
+            // If game mode is sprinkles, make the div random color
             } else if (mode === "sprinkles") {
                 let color = RANDOM_COLORS[Math.floor(Math.random() * RANDOM_COLORS.length)]
                 item.style.backgroundColor = color
+
             } 
             
         })
@@ -163,7 +212,7 @@ function hoverColor(mode) {
 }
 
 /**
- * 
+ * Set grid size when page is loaded
  */
 function pageLoad() {
     mediumGrid(currentMode)
