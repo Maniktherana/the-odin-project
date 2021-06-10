7f8ed49df0189954e7cde86b4756811be26339f8
Manik <manikrana54@gmail.com>
2021-06-10 22:28:34 +0530

    Add some Input functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,7 +1,28 @@
 const calculator = document.querySelector('.calculator-container')
 const keys = calculator.querySelector('.button-container')
+const display = calculator.querySelector('.current-calculation')
 
+function updateDisplay(e) {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
 
+        if (display.textContent === '0') {
+            display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
+        if (action === 'decimal') {
+            if(displayedNum.includes('.')) {
+                display.textContent = displayedNum
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+    }
+}
 
 
 keys.addEventListener('click', e => {
@@ -16,8 +37,8 @@ keys.addEventListener('click', e => {
             console.log('clear key')
         } if (action === 'delete') {
             console.log('delete key')
-        } if (action === 'calculate') {
-            console.log('calculate key')
+        } if (action === 'equals') {
+            console.log('equals key')
         } if (
             action === 'add' ||
             action === 'subtract' ||
@@ -29,3 +50,5 @@ keys.addEventListener('click', e => {
         }
     }
 })
+
+keys.addEventListener('click', updateDisplay)
