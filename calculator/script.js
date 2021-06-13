ea6218f5d476699b0666e4a7d8dc6d8226907e0c
Manik <manikrana54@gmail.com>
2021-06-13 14:22:51 +0530

    Revert "Add guard clauses"
    This reverts commit b0ccf2a9a7a6a630e0f44036124593afdec43bb1.

--- calculator/script.js
+++ calculator/script.js
@@ -9,29 +9,38 @@ function updateDisplay(e) {
         const keyContent = key.textContent
         const displayedNum = display.textContent
 
-        if (displayedNum === '0') display.textContent = keyContent
-        display.textContent = displayedNum + keyContent
-        
-        if (action === 'decimal')
-        if(displayedNum.includes('.')) display.textContent = displayedNum
-        display.textContent = displayedNum + keyContent
-        
-        if (action === 'delete') 
-        if (displayedNum !== '0' && displayedNum.length !== 1) display.textContent = displayedNum.substring(0, displayedNum.length - 1);
-        display.textContent = '0'
-
-        if (action === 'clear') display.textContent = '0'
-        
+        if (displayedNum === '0') {
+            display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
+        if (action === 'decimal') {
+            if(displayedNum.includes('.')) {
+                display.textContent = displayedNum
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+        if (action === 'delete') {
+            if (displayedNum !== '0' && displayedNum.length !== 1) {
+                display.textContent = displayedNum.substring(0, displayedNum.length - 1);
+            } else {
+                display.textContent = '0'
+            }
+        }
+        if (action === 'clear') {
+            display.textContent = '0'
+        }
     }
 }
 
-// logs clicks to console
+
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target
         const action = key.dataset.action
         if (!action) {
-            console.log(key.textContent)
+            console.log('number key')
         } if (action === 'decimal') {
             console.log('decimal key')
         } if (action === 'clear') {
@@ -52,5 +61,4 @@ keys.addEventListener('click', e => {
     }
 })
 
-// Event listener
 keys.addEventListener('click', updateDisplay)
